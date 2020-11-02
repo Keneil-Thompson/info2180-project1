b0d040d1a04c4dc481e5be5bc94eaee7bb32b4ba
Keneil Thompson <kjay_thompson@outlook.com>
2020-11-01 23:49:11 -0500

    Modified files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+function ValidateEmail(email)
+{
+var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("Entered Email address is invalid!");
+document.form1.text1.focus();
+return false;
+}
+}
